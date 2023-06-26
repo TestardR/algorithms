3bc0907330958683ecd9028f42bbbf251cff3e9a
TestardR <romain.rtestard@gmail.com>
2023-06-26 08:55:28 +0200

    add heap algorithms

---
+++ neetcode/9_heap_priority_queue/3_k_closest_points.py
@@ -0,0 +1,16 @@
+import heapq
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        pts = []
+        for x, y in points:
+            dist = (x ** 2) + (y ** 2)
+            pts.append([dist, x, y])
+            
+        res = []
+        heapq.heapify(pts)
+        for _ in range(k):
+            dist, x, y = heapq.heappop(pts)
+            res.append([x, y])
+            
+        return res
